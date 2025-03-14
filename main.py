082ac7d9d5675e1056f86012f2babd10b630d884
Koppany <bkoppany6050@gmail.com>
2025-03-14 10:26:30 +0100

    mini_batch

--- main.py
+++ main.py
@@ -11,18 +11,20 @@ class Network(object):
     
     def feedforward(self, a):
         for b, w in zip(self.biases, self.weights):
-            a = sigmoid(np.dot(w, a)+ b)
+            a = self.sigmoid(np.dot(w, a)+ b)
     
+
+    #feldarabolja az inputokat kis csoportokra és mindegyikre elvégzik a műveleteket
     def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
         if test_data: 
             n_test = len(test_data)
         n = len(training_data)
         for j in range(epochs): 
-            random.shuffle(training_data)
+            random.shuffle(training_data) #randomizálja a kódot
             mini_batches = [
                 training_data[k:k + mini_batch_size]
                 for k in range(0, n, mini_batch_size)
-            ]
+            ]  #felbontja a listát megfelelő angysárúra ebben az esetben az adat tömbünket
             for mini_batch in mini_batches: 
                 self.update_mini_batch(mini_batch, eta)
             if test_data: 
@@ -31,5 +33,12 @@ class Network(object):
                 print ("Epoch {0} complete"-format(j))
 
 
-def sigmoid(z):
-    return 1.0 / (1.0 + np.exp(-z))
+    def sigmoid(z):
+        return 1.0 / (1.0 + np.exp(-z))
+
+    def update_mini_batch(self, mini_batch, eta): 
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch: 
+            delta_nabla_b, delta_nabla_w = self.backprop(x,y)
+            
