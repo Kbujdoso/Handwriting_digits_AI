4f65c2f110c7da3ae4652a938e5c4de220fc7964
Koppany <bkoppany6050@gmail.com>
2025-03-14 10:41:53 +0100

    update_mini_batch finished

--- main.py
+++ main.py
@@ -35,10 +35,20 @@ class Network(object):
 
     def sigmoid(z):
         return 1.0 / (1.0 + np.exp(-z))
-
+#nabla matematikai kifejezés a gradiánsra 
     def update_mini_batch(self, mini_batch, eta): 
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         for x, y in mini_batch: 
             delta_nabla_b, delta_nabla_w = self.backprop(x,y)
-            
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w-(eta/len(mini_batch))*nw
+                        for w, nw in zip(self.weights,nabla_w)]
+        self.biases = [b - (eta/len(mini_batch)) * nb
+                       for b, nb in zip(self.weights, nabla_b)]
+
+
+#backprop
+
+
